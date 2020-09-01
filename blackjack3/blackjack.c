e6c8288d02bb55f3021f87d943d3b63b1421d6c2
Jay Cooper <jay7cooper@gmail.com>
2020-09-01 14:09:43 +1000

    Seemed to have fixed the 32767 issue. Math still not correct though but closer

--- blackjack3/blackjack.c
+++ blackjack3/blackjack.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+//#include <math.h>
 #include <time.h>
 
 int betting(int, int);
@@ -8,14 +8,11 @@ void choice(char, int);
 
 int main(void)
 {
-    int bet, funds, win, loss, record, winStreak, playerTotal, dealerTotal = 0;
-//    char choice = ' ';
-    char answer = ' ';
+    int bet, funds/*, win, loss, record, winStreak, playerTotal, dealerTotal*/ = 0;    
     time_t t;
     srand((unsigned)time(&t));
-    int cards = rand() % 13 + 1;
 
-    //Write code to clear terminal when the game starts
+    /*Write code to clear terminal when the game starts*/
     printf("Welcome to the Blackjack table.\n");
     printf("How much would you like to deposit?\n$");
     scanf("%d", &funds);
@@ -24,19 +21,20 @@ int main(void)
         printf("You have $%d available. How much would you like to bet?\n$", funds);
         scanf("%d", &bet);
         betting(bet, funds);
-        //Add code for dealer hands so it deals and then calculates who wins
-        //It will have to work in between player hands so you can decide whether to hit or stand, etc
+        /*Add code for dealer hands so it deals and then calculates who wins
+        It will have to work in between player hands so you can decide whether to hit or stand, etc*/
     }
     else {
         printf("You have no money left.\n\nSecurity will now escort you out of the casino.\n\n");
     }
-
     return 0;
 }
 
 void choice(char answer, int playerTotal) {
     int pTotal = playerTotal;
-    int newPlayerCard = 0;
+    int newPlayerCard;
+    time_t t;
+    srand((unsigned)time(&t));
 
     switch(answer) {
         case 'H':
@@ -55,11 +53,22 @@ void choice(char answer, int playerTotal) {
                 printf("K\n");
                 newPlayerCard = 10;
             }
-            else if (newPlayerCard == 1) {
+            else if (newPlayerCard == 1 && pTotal <= 10) {
                 printf("A\n");
+                newPlayerCard = 11;
+
+//                if (playerDraw1 + playerDraw2 <= 10) {
+//                    playerDraw2 = 11;
+//                    playerTotal = playerDraw1 + playerDraw2;
+//                    printf("%d A\n", playerDraw1);
+//                } else {
+//                    playerDraw2 = 1;
+//                    playerTotal = playerDraw1 + playerDraw2;
+//                    printf("%d A\n", playerDraw1);
+//                }
                 // add code for 1 or 11
                 // see line 136
-                // if more than one Ace, the second counts as 1
+                // if more than one Ace, all other aces count as 1
                 // If an Ace is eleven but you draw over 21, it reverts back to 1
             }
             else {
@@ -72,15 +81,13 @@ void choice(char answer, int playerTotal) {
         case 's':
             // Add code to go to dealers draw function
             break;
-
     }
-
-//    return pTotal;
-//    return answer;
 }
 
 int betting(int bet, int funds) {
-    int /*cards,*/ playerTotal, playerDraw1, playerDraw2 = 0;
+    int playerTotal;
+    int playerDraw1;
+    int playerDraw2;
     char answer = ' ';
 
     while (bet > funds) {
@@ -93,52 +100,65 @@ int betting(int bet, int funds) {
 
     // If both cards are a JQK or A then the system breaks
     if (playerDraw1 == 11) {
+        playerTotal = 10 + playerDraw2;
+        // playerDraw1 = 10;
         printf("J %d\n", playerDraw2);
-        playerDraw1 = 10;
+        printf("Player total:\n%d\n", playerTotal);
     }
     else if (playerDraw2 == 11) {
+        playerTotal = 10 + playerDraw1;
+        //playerDraw2 = 10;
         printf("%d J\n", playerDraw1);
-        playerDraw2 = 10;
+        printf("Player total:\n%d\n", playerTotal);
     }
     else if (playerDraw1 == 12) {
+        playerTotal = 10 + playerDraw1;
+        //playerDraw1 = 10;
         printf("Q %d\n", playerDraw2);
-        playerDraw1 = 10;
+        printf("Player total:\n%d\n", playerTotal);
     }
     else if (playerDraw2 == 12) {
+        playerTotal = 10 + playerDraw2;
+        //playerDraw2 = 10;
         printf("%d Q\n", playerDraw1);
-        playerDraw2 = 10;
+        printf("Player total:\n%d\n", playerTotal);
     }
     else if (playerDraw1 == 13) {
+        playerTotal = 10 + playerDraw1;
+        //playerDraw1 = 10;
         printf("K %d\n", playerDraw2);
-        playerDraw1 = 10;
+        printf("Player total:\n%d\n", playerTotal);
     }
     else if (playerDraw2 == 13) {
+        playerTotal = 10 + playerDraw2;
+        //playerDraw2 = 10;
         printf("%d K\n", playerDraw1);
-        playerDraw2 = 10;
+        printf("Player total:\n%d\n", playerTotal);
     }
     else if (playerDraw1 == 1) {
-
         if (playerDraw1 + playerDraw2 <= 10) {
             playerDraw1 = 11;
+            playerTotal = playerDraw1 + playerDraw2;
             printf("A %d\n", playerDraw2);
-            //add total?
-        }
-        else {
-            playerDraw1 = 1;
+            printf("Player total:\n%d\n", playerTotal);
+        } else {
+            playerDraw2 = 1;
+            playerTotal = playerDraw1 + playerDraw2;
             printf("A %d\n", playerDraw2);
-            // add total?
+            printf("Player total:\n%d\n", playerTotal);
         }
     }
     else if (playerDraw2 == 1) {
-
         if (playerDraw1 + playerDraw2 <= 10) {
             playerDraw2 = 11;
             playerTotal = playerDraw1 + playerDraw2;
             printf("%d A\n", playerDraw1);
+            printf("Player total:\n%d\n", playerTotal);
         } else {
             playerDraw2 = 1;
             playerTotal = playerDraw1 + playerDraw2;
             printf("%d A\n", playerDraw1);
+            printf("Player total:\n%d\n", playerTotal);
         }
     }
     else {
