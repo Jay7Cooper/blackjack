c8a57fc24919f64ecef684e02eb1131cb673e0db
Jay Cooper <jay7cooper@gmail.com>
2020-09-28 14:53:26 +1000

    Find out why 3rd card doesn't add to total

---
+++ testcode/blackjack/cmake-build-debug/CMakeFiles/blackjack.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/blackjack.dir/blackjack.c.o"
+  "blackjack"
+  "blackjack.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/blackjack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
